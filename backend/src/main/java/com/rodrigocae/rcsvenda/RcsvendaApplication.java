c96f5f0d302b5e5216539008e342b3ad2aefb168
rodrigoCae <rodrigocae@gmail.com>
2021-09-11 01:50:14 -0300

    Project created

---
+++ backend/src/main/java/com/rodrigocae/rcsvenda/RcsvendaApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigocae.rcsvenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcsvendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcsVendaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rodrigocae/rcsvenda/RcsVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigocae.rcsvenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcsVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
