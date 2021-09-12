bd70b525ee0a03ae9e7ecf103139d46d5907f175
rodrigoCae <rodrigocae@gmail.com>
2021-09-12 00:35:27 -0300

    basic static component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+
 function App() {
   return (
+    <>
+    <NavBar/>
     <div className="App">
       <h1 className="text-primary">E AE!!!</h1>
+      
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
