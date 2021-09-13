7a8629a6869245926cf279f7db83c614e8c2b944
rodrigoCae <rodrigocae@gmail.com>
2021-09-12 23:32:32 -0300

    Layers

---
+++ backend/src/main/java/com/rodrigocae/rcsvenda/services/SellerService.java
@@ -0,0 +1,24 @@
+package com.rodrigocae.rcsvenda.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigocae.rcsvenda.dto.SellerDTO;
+import com.rodrigocae.rcsvenda.entities.Seller;
+import com.rodrigocae.rcsvenda.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+	
+}
