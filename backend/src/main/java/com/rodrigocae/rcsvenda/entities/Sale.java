c131473d48f03f797bf0292cece764ffeeeeeb79
rodrigoCae <rodrigocae@gmail.com>
2021-09-12 21:00:08 -0300

    Domain Model, database Seed

---
+++ backend/src/main/java/com/rodrigocae/rcsvenda/entities/Sale.java
@@ -0,0 +1,43 @@
+package com.rodrigocae.rcsvenda.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name= "tbl_sales")
+public class Sale {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE )
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Seller seller;
+	
+	public Sale() {}
+
+	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
+		super();
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+	
+	
+}
